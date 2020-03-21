d955ef786e0a1a95da90b29bb9eae55b7654945a
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2020-03-21 15:41:48 -0400

    init header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1>Antoine Barbier</h1>
+        <nav class="menu">
+            <a href="" >Home</a>
+            <a href="" >Parcours</a>
+            <a href="" >Compétences</a>
+            <a href="" ></a>
+            <a href="" ></a>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+
+</style>
